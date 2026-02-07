1f6e864ebd7d20a4eb0a8b4fd07f9a36545a2f7e
Stacy Brock <kalrnux@gmail.com>
2026-02-07 15:22:26 -0800

    Add support for watching multiple repos

--- README.md
+++ README.md
@@ -7,6 +7,18 @@ Send a [Pushover](https://pushover.net) notification when a new release is added
 - Python 3.11+
 - PyGithub
 
+In addition, a Pushover application must be created and configured in the [Pushover](https://pushover.net) control panel.
+
 ## Configuration
 
-Configure via environment variables
+Configuration is via environment variables:
+
+|Config Option|Purpose|
+-|-
+`RELEASEWATCH_TARGET_REPOS`|comma-separated list of repo names (ex: vim/vim)
+`RELEASEWATCH_GITHUB_TOKEN`|GitHub PAT token
+`RELEASEWATCH_PUSHOVER_APP_KEY`|Pushover application key
+`PUSHOVER_DEVICE`|Pushover device name to send notifications to
+`PUSHOVER_USER_KEY`|Pushover user key
+
+**All** configuration options are **required**.
--- releasewatch.py
+++ releasewatch.py
@@ -1,6 +1,7 @@
+import json
 import os
 import requests
-from github import Github, Auth
+from github import Github, Auth, UnknownObjectException
 
 import pprint
 pp_ = pprint.PrettyPrinter(indent=2)
@@ -9,7 +10,7 @@ pp = pp_.pprint
 SCRIPTPATH = os.path.dirname(os.path.realpath(__file__))
 
 CONFIG = {}
-CONFIG['TARGET_REPO'] = os.getenv('RELEASEWATCH_TARGET_REPO', None)
+CONFIG['TARGET_REPOS'] = os.getenv('RELEASEWATCH_TARGET_REPOS', None).split(',')
 CONFIG['GITHUB_TOKEN'] = os.getenv('RELEASEWATCH_GITHUB_TOKEN', None)
 CONFIG['PUSHOVER_APP_KEY'] = os.getenv('RELEASEWATCH_PUSHOVER_APP_KEY', None)
 CONFIG['PUSHOVER_DEVICE'] = os.getenv('PUSHOVER_DEVICE', None)
@@ -18,9 +19,8 @@ CONFIG['PUSHOVER_USER_KEY'] = os.getenv('PUSHOVER_USER_KEY', None)
 def main():
     auth = Auth.Token(CONFIG['GITHUB_TOKEN'])
     gh = Github(auth=auth)
-    repo = gh.get_repo(CONFIG['TARGET_REPO'])
 
-    # read last notified patch value from file
+    # read last notified values from file
     try:
         with open(SCRIPTPATH + '/last_notified.cache', 'r') as CACHEFILE:
             raw = CACHEFILE.read()
@@ -28,29 +28,39 @@ def main():
         raw = ''
 
     if raw == '':
-        notified_release = ''
+        notified_releases = {}
     else:
-        notified_release = raw.strip()
+        notified_releases = json.loads(raw)
 
-    # get most recent release on repo
-    last_release = repo.get_latest_release()
+    for repo_name in CONFIG['TARGET_REPOS']:
+        repo = gh.get_repo(repo_name)
 
-    # check if release has changed since the last notified release
-    if last_release.name != notified_release:
-        # create pushover notification
-        title = f"{repo.full_name} released {last_release.name}"
-        msg = (f"{repo.full_name} has released '{last_release.name}': "
-               f"{last_release.html_url}")
-        r = requests.post('https://api.pushover.net/1/messages.json', data = {
-            'token': CONFIG['PUSHOVER_APP_KEY'],
-            'user': CONFIG['PUSHOVER_USER_KEY'],
-            'message': msg,
-            'title': title,
-            'device': CONFIG['PUSHOVER_DEVICE']
-        })
+        # get most recent release on repo
+        try:
+            last_release = repo.get_latest_release()
+        except UnknownObjectException:
+            # repo has no releases
+            continue
 
-        with open(SCRIPTPATH + '/last_notified.cache', 'w') as CACHEFILE:
-            CACHEFILE.write(last_release.name)
+        # check if release has changed since the last notified release
+        if last_release.name != notified_releases.get(repo.full_name):
+            # create pushover notification
+            title = f"{repo.full_name} released {last_release.name}"
+            msg = (f"{repo.full_name} has released '{last_release.name}': "
+                   f"{last_release.html_url}")
+            r = requests.post('https://api.pushover.net/1/messages.json', data={
+                'token': CONFIG['PUSHOVER_APP_KEY'],
+                'user': CONFIG['PUSHOVER_USER_KEY'],
+                'message': msg,
+                'title': title,
+                'device': CONFIG['PUSHOVER_DEVICE']
+            })
+
+            notified_releases[repo.full_name] = last_release.name
+
+    # write last notified values to file
+    with open(SCRIPTPATH + '/last_notified.cache', 'w') as CACHEFILE:
+        CACHEFILE.write(json.dumps(notified_releases))
 
 if __name__ == '__main__':
     main()
